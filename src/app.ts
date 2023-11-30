f201ad3a6d8466333772ff590f602e2dbe7df4a3
essien emmanuel <essienemma300@gmail.com>
2023-11-30 11:54:10 +0100

    worked on organizing the project and adding webpack

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,8 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="utils/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="components/project-item.ts" />
-/// <reference path="components/project-list.ts" />
-/// <reference path="components/project-input.ts" />
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
 
-namespace App {
-  //PROJECT TYPE
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
 
-  //PROJECT STATE MANAGEMENT
-  
-
-
-  //COMPONENT BASE CLASS
-  
-
-  //PROJECT ITEM CLASS
-  
-
-  //PROJECT  LIST CLASS
-  
-
-  //PROJECT INPUT CLASS
-  
-
-  new ProjectInput();
-  new ProjectList("active");
-  new ProjectList("finished");
-}
+console.log('h9')
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,48 +1,45 @@
-/// <reference path="base.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../models/drag-drop.ts" />
-/// <reference path="../models/project.ts" />
+import { Component } from './base';
+import { Draggable } from '../models/drag-drop';
+import { AutoBind } from '../decorators/autobind';
+import { Project } from '../models/project'
 
-namespace App {
-  export class ProjectItem
-    extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable
-  {
-    private project: Project;
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private project: Project;
 
-    get persons() {
-      if (this.project.people === 1) return "1 person";
-      else return `${this.project.people} persons`;
-    }
+  get persons() {
+    if (this.project.people === 1) return "1 person";
+    else return `${this.project.people} persons`;
+  }
 
-    constructor(hostId: string, project: Project) {
-      super("single-project", hostId, false, project.id);
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
 
-      this.project = project;
+    this.project = project;
 
-      this.configure();
-      this.attachContent();
-    }
+    this.configure();
+    this.attachContent();
+  }
 
-    @AutoBind
-    dragStartHandler(event: DragEvent): void {
-      event.dataTransfer!.setData("text/plain", this.element?.id);
-      event.dataTransfer!.effectAllowed = "move";
-    }
+  @AutoBind
+  dragStartHandler(event: DragEvent): void {
+    event.dataTransfer!.setData("text/plain", this.element?.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
 
-    @AutoBind
-    dragEndHandler(_event: DragEvent): void {}
+  @AutoBind
+  dragEndHandler(_event: DragEvent): void {}
 
-    configure() {
-      this.element.addEventListener("dragstart", this.dragStartHandler);
-      this.element.addEventListener("dragend", this.dragEndHandler);
-    }
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
 
-    attachContent() {
-      this.element.querySelector("h2")!.textContent = this.project.title;
-      this.element.querySelector("h3")!.textContent =
-        this.persons + " assigned";
-      this.element.querySelector("p")!.textContent = this.project.description;
-    }
+  attachContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
   }
 }
